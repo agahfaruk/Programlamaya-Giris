7ccf2e83a53e4f5295507dec3d32ae5b8311eeb2
Agah Faruk Küçük <91697999+agahfaruk@users.noreply.github.com>
2025-04-22 22:41:40 +0300

    Add files via upload

---
+++ Döngüler (for)/Döngüler (for).cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+
+	int tekToplam = 0;
+	int tekSayac = 0;
+
+	int ciftToplam = 0;
+	int ciftSayac = 0;
+
+	for (int i = 0; i <= 1000; i++) {
+		cout << i;
+
+		if (i % 2 == 0) {
+			ciftSayac++;
+			ciftToplam += i;
+		}
+		else {
+			tekSayac++;
+			tekToplam += i;
+		}
+	}
+	cout << "\n" "\n";
+
+	cout << "Tek Sayisi : " << tekSayac << endl;
+	cout << "Cift Sayisi : " << ciftSayac << endl;
+
+	cout << "Tek Toplam : " << tekToplam << endl;
+	cout << "Cift Toplam : " << ciftToplam << endl;
+
+	cout << "Tek Ortalama : " << tekToplam / tekSayac << endl;
+	cout << "Cift Ortalama : " << ciftToplam / ciftSayac << endl;
+
+	return 0;
+}
---
+++ Harf notu hesaplayıcı/Harf notu hesaplayıcı.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+    float ort;
+    cout << "Ortalama Giriniz : ";
+    cin >> ort;
+
+    if (ort >= 90)
+        cout << "AA";
+    else if (ort >= 80)
+        cout << "AB";
+    else if (ort >= 70)
+        cout << "BB";
+    else if (ort >= 60)
+        cout << "BC";
+    else if (ort >= 50)
+        cout << "CC";
+    else if (ort >= 40)
+        cout << "CD";
+    else if (ort >= 30)
+        cout << "DD";
+    else if (ort >= 20)
+        cout << "DF";
+    else if (ort >= 10)
+        cout << "FF";
+    else if (ort >= 0)
+        cout << "FF";
+
+    return 0;
+}
