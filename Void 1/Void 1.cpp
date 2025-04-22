a8be44eefa585af5e06f840afd80e6a23da9e57b
Agah Faruk Küçük <91697999+agahfaruk@users.noreply.github.com>
2025-04-22 22:39:45 +0300

    Add files via upload

---
+++ Void 1/Void 1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+char harf;
+
+void basHarfi() {
+    
+    for (int i = 1; i <= 20; i++) {
+        cout << harf << endl;
+    }
+    
+}
+
+int main()
+{
+    
+    cout << "Bas harfi 20 kez yazilacak kelimeyi giriniz : ";
+    cin >> harf;
+
+    cout << "iste 20 defa kelimenin bas harfi (" << harf << ")" << endl;
+    basHarfi();
+
+
+
+    return 0;
+}
---
+++ Void 2/Void 2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+void mesajYazdir(int tekrarSayisi);
+
+int main()
+{
+    int girilenSayi;
+
+    cout << "Kac defa mesaj almak istiyorsunuz? : ";
+    cin >> girilenSayi;
+    mesajYazdir(girilenSayi);
+
+
+    return 0;
+}
+
+void mesajYazdir(int tekrarSayisi) {
+    for (int i = 1; i <= tekrarSayisi; i++) {
+        cout << "Mesaj " << i << endl;
+    }
+}
